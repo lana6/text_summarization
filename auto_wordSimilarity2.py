83186ef3c68aa6974b7a11fde4a5fbb03e19b1df
lana6 <lana_d@btinternet.com>
2012-06-28 19:32:10 +0100

    Alterations to tot_grouped_nodes algorithm in auto_wordSimilarity2.py

--- auto_wordSimilarity2.py
+++ auto_wordSimilarity2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from __future__ import division  
 import itertools
 import copy 
-from itertools import *
+#from itertools import *
+import decimal
 import re
 import math
 import networkx as nx
@@ -44,7 +46,10 @@ for w1, w2 in itertools.combinations(refined_file_lines, 2):
       if e1 == e2:  
         common_words.append(e1)
    total_common_words.append(common_words)
-   wight = len(set(common_words)) / (math.log(len(w1),10) + math.log(len(w2),10))
+   #w1_len_Decimal = decimal.Decimal(len(w1))
+   #w2_len_Decimal = decimal.Decimal(len(w2))
+   #wight = len(set(common_words)) / w1_len_Decimal.log10() + w1_len_Decimal.log10()
+   wight = len(set(common_words)) / (math.log(len(w1),10) + (math.log(len(w2),10)))   
    weighted_sen.append(wight)
    ele_name = [] # create a new list  
    if wight != 0.0:
@@ -63,18 +68,18 @@ for w1, w2 in itertools.combinations(refined_file_lines, 2):
 grouped_nodes= []
 tot_grouped_nodes = []
 var = 1
+## this algorithm assumes that triplets of tot_element_names with the same first element/node 
+## are next to each other in tot_element_names
 for (i,j,x) in tot_element_names:   
-    index1 = tot_element_names.index([i,j,x]) # current index
-    if var == tot_element_names[index1][0]:
-     grouped_nodes.append([i,j,x]) # linked nodes with with weights info, grouped by [index][0](sentence)
-    if var  != tot_element_names[index1][0]:
+    if var == i:
+        grouped_nodes.append([i,j,x]) # linked nodes with with weights info, grouped by [index][0](sentence)
+    else:
         tot_grouped_nodes.append(grouped_nodes) # Allows each sentence list to be separated
         grouped_nodes = []
         grouped_nodes.append([i,j,x])
-        var += 1 
-grouped_nodes = []  
-grouped_nodes.append([i,j,x])
-tot_grouped_nodes.append(grouped_nodes)       
+        var = i
+## final grouped_nodes has not yet been appended, so do it now
+tot_grouped_nodes.append(grouped_nodes)
 
 
 # Each list group created above needs to contain lists of a11 nodes linked to it.  This function appends linked lists where necessary 
@@ -95,7 +100,7 @@ for i in range(0, var_len):
              if flag == "negative": # Ensures that only one list is copied into the new group
                  temp_list = cpy_grouped_nodes[i][j] # Identifies list to be copied                
                  temp_list2 = []
-                 temp_list2.append(temp_list[1])
+                 temp_list2.append(temp_list[1]) # append takes one arguement
                  temp_list2.append(temp_list[0])
                  temp_list2.append(temp_list[2])
                  tot_grouped_nodes[i2].extend([temp_list2]) # List appened to group                
@@ -121,50 +126,64 @@ for i in range(0, var_len):
   list_index +=1  
 
 
-#Using networkx to create a graph of linked nodes
+# Set the initial score for each sentence. ie (1 - d) where d is the damping factor of 0.85
 
-G = nx.Graph()
+str_damping_factor = str(0.15)
+d = decimal.Decimal(str_damping_factor)
+sentence_score = [1,1,1,0.15]
+#sentence_score.append([0.15]) 
 
-pos=nx.spring_layout(G)
+for i in tot_grouped_nodes:
+   i.extend([sentence_score])
 
-total_var = []
 
-for index in range(len(file_lines)):
-    var = "S" + str(index + 1)
-    G.add_node(var)
-    total_var.append(var)
+# Evaluate ranks for sentences
 
-for index in range(len(tot_element_names)):
-     if tot_element_names[index]:
-       var = 'S' + str(tot_element_names[index][0])
-       var1 = 'S' + str(tot_element_names[index][1])
-       var3 = tot_element_names[index][2]
-       G.add_edge(var,var1,{'weight':var3})
+cpy_grouped_nodes = []
+cpy_grouped_nodes = copy.deepcopy(tot_grouped_nodes) # Copy of original created
+var_len = len(cpy_grouped_nodes)
+sum_weights = 0
+list_index = 0
+flag = "negative"
+sub_score = 0.0000
+tot_score = 0.0000
 
-print ""
-print ""
-print G.edges()
-print ""
-print ""
-print G.nodes()
+for i in range(0, var_len):
+   tot_score = 0
+   sub_score = 0
+   for j in range(0, len(cpy_grouped_nodes[i])): 
+     for i2 in range(0, var_len):
+         flag = "negative"  
+         print flag , "outside i2"
+         for j2 in range(0, len(cpy_grouped_nodes[i2])):
+           #if cpy_grouped_nodes[i][j] == cpy_grouped_nodes[i2][j2]:
+              print flag , "j2"                  
+              if flag == "negative":
+               decimal_weight1 = cpy_grouped_nodes[i][j][2]
+               decimal_weight2 = cpy_grouped_nodes[i][j][3]
+               print cpy_grouped_nodes[i][j][2] , "nom" , cpy_grouped_nodes[i][j][3] , "dom" , cpy_grouped_nodes[i][j]
+               if decimal_weight2 != 0:
+                sub_score = round(decimal_weight1 / decimal_weight2, 6)
+                print decimal_weight1 / decimal_weight2 , "direct cal"
+                node_len = len(cpy_grouped_nodes[i2])
+                previous_score = round(tot_grouped_nodes[i2][node_len - 1][3],6)
+                #print tot_grouped_nodes[i2][node_len - 1][3]
+                print previous_score , "previous score" , sub_score , "sub_score"
+                #print int(previous_score) , "int(previous_score)"
+                #sub_score = sub_score * int(previous_score)
+                sub_score = sub_score * previous_score 
+                print sub_score , "sub_score"
+              flag = "positive"
+   tot_score += sub_score
+   print ""
+   print tot_score , "total"
+   print ""
+   print sub_score , "sub_score2"       
+   node_len2 = len(cpy_grouped_nodes[i])  
+   tot_grouped_nodes[i][j][3] = tot_score
+   
 
-nx.draw_circular(G,node_color='m',node_size=500)
 
-plt.show()
-print ""
-print ""
-print nx.pagerank(G)
 
-pagerank_values = nx.pagerank(G)
-
-print pagerank_values
-print ""
-print pagerank_values.keys()
-print ""
-print pagerank_values.values()
-
-for index in pagerank_values:
-  print ""
-  print index
 
 
